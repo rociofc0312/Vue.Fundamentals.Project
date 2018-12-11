22863bc194887d2b0167f49d9d38dd04d36d8493
Rocío Daniela Fernandez Canales <rofernandez@belatrix.com>
2018-12-11 10:44:43 -0500

    First commit

---
+++ src/components/build/RobotBuilder.vue
@@ -0,0 +1,47 @@
+<template src="./RobotBuilder.html"></template>
+<style src="./RobotBuilder.css"></style>
+
+<script>
+import availableParts from '../../data/parts';
+
+function getPreviousValidIndex(index, lenght) {
+  const deprecatedIndex = index - 1;
+  return deprecatedIndex < 0 ? lenght - 1 : deprecatedIndex;
+}
+
+function getNextValidIndex(index, lenght) {
+  const incrementedIndex = index + 1;
+  return incrementedIndex > lenght - 1 ? 0 : incrementedIndex;
+}
+
+export default {
+  name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts,
+      selectedHeadIndex: 0,
+    };
+  },
+  computed: {
+    selectedRobot() {
+      return {
+        head: availableParts.heads[this.selectedHeadIndex],
+      };
+    },
+  },
+  methods: {
+    selectNextHead() {
+      this.selectedHeadIndex = getNextValidIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length,
+      );
+    },
+    selectPreviousHead() {
+      this.selectedHeadIndex = getPreviousValidIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length,
+      );
+    },
+  },
+};
+</script>
---
+++ src/components/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div>
+      <img src="../../assets/home-images/robot-home.png" alt="" class="robot">
+    </div>
+    <div>
+      <a href="">Get started</a> building your fist robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot{
+  height: 300px;
+}
+</style>
